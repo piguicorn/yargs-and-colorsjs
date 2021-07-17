d3a444ebfa80ce0d3ad7c49ac92905dedfe8141f
piguicorn <hello.piguicorn@gmail.com>
2021-07-17 21:51:01 +0200

    colors

--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
+const colors = require('colors');
 
 const crear_archivo = async (base = 1, listar = false) => {
     let salida = '';
     try {
         for (let i = 1; i <= 10; i++) {
-            salida += `${base} x ${i} = ${base * i}\n`;
+            salida += `${base} x ${i} = ${(base * i).toString().cyan} \n`;
         }
     
         fs.writeFileSync(`tabla-${base}.txt`, salida);
